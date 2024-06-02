1220ea0186965fce21600d54ea4d64cb13b296b0
Richard <riicchhaarrd@users.noreply.github.com>
2024-06-02 10:24:37 +0200

    move

--- stddef.h
+++ stddef.h
@@ -2,11 +2,4 @@
 #define STDDEF_H
 #define NULL 0
 
-int main(int argc, char **argv);
-void _start()
-{
-	int status = main(0, NULL);
-	/* __builtin_trap(); */
-	exit(status);
-}
 #endif
--- stdlib.h
+++ stdlib.h
@@ -14,6 +14,14 @@ asm("exit:\n\
 
 __attribute__((__cdecl__,noreturn)) void exit(int status);
 
+int main(int argc, char **argv);
+void _start()
+{
+	int status = main(0, NULL);
+	/* __builtin_trap(); */
+	exit(status);
+}
+
 /* void exit(int code) */
 /* { */
 /*     syscall(SYS_exit, code); */
